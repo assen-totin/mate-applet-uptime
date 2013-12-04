39c4e20c3a497497cbc141e90c39e9a577af8274
assen <assen@2606a3fd-1764-4de1-b3a9-829aeebcb3ac>
2013-12-04 11:12:10 +0000

    Got uptime.

--- src/applet.h
+++ src/applet.h
@@ -14,46 +14,26 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  *  USA.
  *
- *  Lock Keys (formerly Led) Applet was writen by Jörgen Scheibengruber <mfcn@gmx.de>
- *  Modifed for MATE by Assen Totin <assen.totin@gmail.com>
+ *  Uptime Applet was writen by Assen Totin <assen.totin@gmail.com>
  *  
  */
 
-#define APPLET_FACTORY "LockkeysAppletFactory"
-#define APPLET_ID "LockkeysApplet"
-#define APPLET_NAME "lockkeys"
-#define APPLET_ICON "applet_lockkeys.png"
-#define APPLET_ICON_CAPS_OFF "applet_lockkeys_capslock_off.xpm"
-#define APPLET_ICON_CAPS_ON "applet_lockkeys_capslock_on.xpm"
-#define APPLET_ICON_NUM_OFF "applet_lockkeys_numlock_off.xpm"
-#define APPLET_ICON_NUM_ON "applet_lockkeys_numlock_on.xpm"
-#define APPLET_ICON_SCROLL_OFF "applet_lockkeys_scrolllock_off.xpm"
-#define APPLET_ICON_SCROLL_ON "applet_lockkeys_scrolllock_on.xpm"
+#define APPLET_FACTORY "UptimeAppletFactory"
+#define APPLET_ID "UptimeApplet"
+#define APPLET_NAME "uptime"
+#define APPLET_ICON "applet_uptime.png"
 #define APPLET_VERSION "1"
 
-enum {
-        CAPSLOCK  = 0,
-        NUMLOCK,
-        SCROLLLOCK
-} Leds;
+#define APPLET_PROC_UPTIME "/proc/uptime"
 
 typedef struct {
         MatePanelApplet *applet;
-        GtkWidget *vbox, *hbox;
-        GtkWidget *num_pix, *scroll_pix, *caps_pix;
-        GtkWidget *show_cb[3];
-        Display *rootwin;
-        int xkbev, xkberr;
-
-        gboolean on[3];
-        gboolean show[3];
-
+        GtkWidget *vbox, *label_top, label_bottom;
+	int uptime;
         GtkDialog *about, *settings;
-#ifdef HAVE_GSETTINGS
-	GSettings *gsettings;
-#endif
-} LedApplet;
+} UptimeApplet;
 
+static const GtkActionEntry applet_menu_actions [] = {
+	{ "About", GTK_STOCK_ABOUT, NULL, "_About", NULL, G_CALLBACK (about_cb) }
+};
 
-#define SCHEMA_PATH "/org/mate/panel/objects/lockkeys/"
-#define APPLET_SCHEMA "org.mate.panel.applet.LockkeysApplet"
